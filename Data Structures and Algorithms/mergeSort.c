98cfa79713f0cc9433113a80b6bea59def298abe
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2016-01-21 10:21:45 +0530

    Merge Sort

--- Data Structures and Algorithms/mergeSort.c
+++ Data Structures and Algorithms/mergeSort.c
@@ -1,33 +1,98 @@
 // Implementation of Merge Sort
+// Used pointers for the sake of using it. they aren't even needed
 
 #include <stdio.h>
+void merge(int, int, int);
+void partition(int, int);
 
-int mergesort(int*, int , int);
+int array[50];
+
 int main (void)
 {
-	int array[40];
+	int *pointer = &array[0];
+	int count;
+	printf("Enter the Number of Elements (max 50)\n");
+	scanf("%d", &count);
 
-	for (int i = 0; i < 40; i++)
+	printf("Enter the Elements\n");
+	for (int i = 0; i < count; i++)
 	{
-		scanf("%d", &array[i]);
+		printf("Element Number %d\n", i + 1);
+		scanf("%d", (pointer + i));
 	}
 
-	int result = mergesort(&array, 0, 39);
+	printf("Unsorted Array\n");
+	for (int i = 0; i < count; i++)
+		printf("%d ",array[i]);
 
-	if (result == 0)
-	{
-		for (int i = 0; i < 40; i++)
-			printf("%d ", &array[i]);		
-	}
+	partition(0, count - 1);
+
+	printf("\nSorted Array\n");
+	for (int i = 0; i < count; i++)
+		printf("%d ",array[i]);
 
 	return 0;
-
 }
 
-int mergesort(int *arr, int start, int end)
+// Function Definitions
+
+void partition(int start, int end)
 {
-	
+	if (start < end)
+	{
+		int m = (start + end)/2;
+		partition(start, m);
+		partition(m + 1, end);
+		merge(start, m , end);
+	}
 
 }
 
+void merge(int start, int m, int end)
+{
 
+	int lengthLeft = m - start + 1;
+	int lengthRight = end - m;
+
+	int leftArray[lengthLeft], rightArray[lengthRight];
+
+	// Copying elements from original array
+	for (int i = 0; i < lengthLeft; i++)
+		leftArray[i] = array[start + i];
+
+	for (int i = 0; i < lengthRight; i++)
+		rightArray[i] = array[m + 1 + i];
+
+	int leftIterate = 0, rightIterate = 0, originalIterate = start;
+
+	while(leftIterate < lengthLeft && rightIterate < lengthRight)
+	{
+		if (leftArray[leftIterate] < rightArray[rightIterate])
+		{
+			array[originalIterate] = leftArray[leftIterate];
+			leftIterate++;
+			originalIterate++;
+		}
+		else
+		{
+			array[originalIterate] = rightArray[rightIterate];
+			rightIterate++;
+			originalIterate++;
+		}
+	}
+
+	while (leftIterate < lengthLeft)
+	{
+		array[originalIterate] = leftArray[leftIterate];
+		leftIterate++;
+		originalIterate++;
+	}
+
+	while (rightIterate < lengthRight)
+	{
+		array[originalIterate] = rightArray[rightIterate];
+		rightIterate++;
+		originalIterate++;
+	}
+
+}
