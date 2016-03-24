741e40a9d11fbf334e524128cd3733b44173fe36
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2016-03-24 11:15:21 +0530

    conditions

---
+++ Python/conditions.py
@@ -0,0 +1,11 @@
+# http://www.programiz.com/python-programming/if-elif-else
+
+num = float(input("Enter a number: "))
+
+if num > 0:
+	print("Positive Number")
+elif num == 0:
+	print("Zero")
+else:
+	print("Negative Number")
+
