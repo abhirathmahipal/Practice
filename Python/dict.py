602217a7d4315a0d2b5bad204cdd6aa90baf9199
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2016-04-02 21:48:12 +0530

    Dictionary Comprehensions

--- Python/dict.py
+++ Python/dict.py
@@ -20,3 +20,13 @@ key_valuepair['Three'] = 'Four'
 print(key_valuepair.keys())
 print(key_valuepair.values())
 
+
+# Dict Comprehension
+# http://stackoverflow.com/questions/1747817/create-a-dictionary-with-list-comprehension-in-python
+# Creates a dictionary with key and value pairs
+mydictionary = {x:x+1 for x in range(10)}
+print(mydictionary)
+
+# Creates a key and value pair with a tuple as the value
+seconddictionary = {x: tuple([x + 1, x + 2, x + 3]) for x in range (5)}
+print(seconddictionary)
