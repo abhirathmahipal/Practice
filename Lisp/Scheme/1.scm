558f37bfd764c3da4591736698f8bcdaadec38f0
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2016-08-02 12:34:37 +0530

    2 scheme examples.

---
+++ Lisp/Scheme/1.scm
@@ -0,0 +1,12 @@
+; https://www.shido.info/lisp/scheme4_e.html
+
+; Hello world variable
+(define vhello "Hello World")
+
+; Hello World as a function
+(define fhello (lambda () "Hello World"))
+
+fhello
+vhello
+
+(fhello)
---
+++ Lisp/Scheme/farg.scm
@@ -0,0 +1,8 @@
+(define hello
+	(lambda (name)
+		(string-append "Hello " name "!")))
+
+; sum of three numbers
+(define sum3
+	(lambda (a b c)
+		(+ a b c)))
