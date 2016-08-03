91dcec532c8c5464c5df4f89ef2c16f08b1295e2
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2016-08-03 15:52:04 +0530

    Added some templates.

--- Python/Miguel Mega Tutorial Flask/app/views.py
+++ Python/Miguel Mega Tutorial Flask/app/views.py
@@ -1,7 +1,19 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Abhirath'}
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
