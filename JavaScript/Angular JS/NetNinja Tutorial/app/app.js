9a33f07951120ce152cd883e3aaf49d3414c7478
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2016-08-15 16:58:27 +0530

    Video 10

--- JavaScript/Angular JS/NetNinja Tutorial/app/app.js
+++ JavaScript/Angular JS/NetNinja Tutorial/app/app.js
@@ -1,13 +1,28 @@
 var app = angular.module('myNinjaApp', []);
 
-app.config(function() {
+app.controller('NinjaController', ['$scope', function($scope) {
+	$scope.message = "Hey";
+	$scope.ninjas = [
+		{
+			name:"Yoshi",
+			belt:"Green",
+			rate:50
+		},
+		{
+			name:"Crystal",
+			belt:"Yellow",
+			rate: 30
+		},
+		{
+			name:"Shaun",
+			belt:"Black",
+			rate: 40
+		},
+		{
+			name:"Naruto",
+			belt:"Black",
+			rate: 100
+		}
+	];
 
-
-
-});
-
-app.run(function() {
-
-
-
-});
+}])
