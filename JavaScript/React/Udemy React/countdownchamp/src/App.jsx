aadb7c66c449fe254690d5cbe6166f1b3fe3de07
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2017-03-04 20:16:18 +0530

    Started with React

---
+++ JavaScript/React/Udemy React/countdownchamp/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>Countdown Champ, App Component</div>
+        )
+    }
+}
+
+export default App;
