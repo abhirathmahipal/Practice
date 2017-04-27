ea8f069fb8f7bfcf0c63a77ac1cc18f2502a38b3
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2017-04-27 07:08:25 +0530

    Done with 48 steps

--- C/Kilo/kilo.c
+++ C/Kilo/kilo.c
@@ -13,6 +13,16 @@
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey {
+    ARROW_LEFT = 'a',
+    ARROW_RIGHT = 'd',
+    ARROW_UP = 'w',
+    ARROW_DOWN = 's'
+};
+
+
+
+
 /*** data ***/
 struct editorConfig {
     int cx, cy;
@@ -58,8 +68,28 @@ char editorReadKey() {
         if (nread == -1 && errno != EAGAIN) die("read");
     }
 
-    return c;
+    if (c == '\x1b') {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+        if (seq[0] == '[') {
+            switch (seq[1]) {
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN;
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+            }
+
+        }
+
+        return '\x1b';
+    } else {
+        return c;
+    }
 }
+
 
 int getCursorPosition(int *rows, int *cols) {
 
@@ -179,16 +209,16 @@ void editorRefreshScreen() {
 
 void editorMoveCursor(char key) {
     switch (key) {
-        case 'a':
+        case ARROW_LEFT:
             E.cx--;
             break;
-        case 'd':
+        case ARROW_RIGHT:
             E.cx++;
             break;
-        case 'w':
+        case ARROW_UP:
             E.cy--;
             break;
-        case 's':
+        case ARROW_DOWN:
             E.cy++;
             break;
     }
@@ -205,10 +235,10 @@ void editorProcessKeypress() {
             exit(0);
             break;
 
-        case 'w':
-        case 's':
-        case 'a':
-        case 'd':
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
     }
