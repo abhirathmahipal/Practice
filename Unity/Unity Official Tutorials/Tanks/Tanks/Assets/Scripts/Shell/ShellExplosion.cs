01c3910952e24f2eba0914d8664a2f645a4f6e0f
Abhirath <abhiabhiabhi.27@gmail.com>
2017-05-05 07:23:34 +0530

    Handle firing shells

--- Unity/Unity Official Tutorials/Tanks/Tanks/Assets/Scripts/Shell/ShellExplosion.cs
+++ Unity/Unity Official Tutorials/Tanks/Tanks/Assets/Scripts/Shell/ShellExplosion.cs
@@ -20,12 +20,57 @@ public class ShellExplosion : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // Find all the tanks in an area around the shell and damage them.
+
+		// Get all the objects that the shell's radius can touch
+		// Onl choose the layers in which the tanks exist.
+		Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+
+		for (int i = 0; i < colliders.Length; i++) {
+			Rigidbody targetRigidbody = colliders [i].GetComponent<Rigidbody> ();
+
+
+			// Just in case we don't find a rigidbody even though we made everything over thre and are sure
+			// it's going to have one
+			if (!targetRigidbody)
+				continue;
+
+			targetRigidbody.AddExplosionForce (m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+			TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth> ();
+
+			if (!targetHealth)
+				continue;
+
+			float damage = CalculateDamage (targetRigidbody.position);
+
+			targetHealth.TakeDamage (damage);
+		
+		}
+
+		m_ExplosionParticles.transform.parent = null;
+		m_ExplosionParticles.Play ();
+
+		m_ExplosionAudio.Play ();
+
+		Destroy (m_ExplosionParticles.gameObject, m_ExplosionParticles.duration);
+		Destroy (gameObject);
     }
 
 
     private float CalculateDamage(Vector3 targetPosition)
     {
         // Calculate the amount of damage a target should take based on it's position.
-        return 0f;
+		Vector3 explosionToTarget = targetPosition - transform.position;
+
+		float explosionDistance = explosionToTarget.magnitude; // finding the distance
+
+		float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+
+
+		float damage = relativeDistance * m_MaxDamage;
+
+		damage = Mathf.Max (0f, damage); // if negative make zero, else let it be
+
+		return damage;
     }
 }
